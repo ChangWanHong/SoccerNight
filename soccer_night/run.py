54f11eecfb696fe2b00bb7f0b59b3339476dd97c
Chang Wan Hong <jourmoon@gmail.com>
2014-02-27 22:58:43 +0900

    Add simple running script.

---
+++ soccer_night/run.py
@@ -0,0 +1,15 @@
+import soccer_night
+from getpass import getpass
+
+def main():
+    id = raw_input("Enter id: ")
+    pw = getpass()
+
+    soccer = soccer_night.SoccerNight(id, pw)
+
+    while True:
+        soccer.confirm_league_match_results()
+
+    soccer.close()
+if __name__ == '__main__':
+    main()
