99930989e2056e07ef945d88f1dd5432982eb126
ChangWanHong <jourmoon@gmail.com>
2014-03-24 23:36:15 +0900

    WIP

--- soccer_night/soccer_night.py
+++ soccer_night/soccer_night.py
@@ -107,6 +107,30 @@ class SoccerNight(object):
     BUTTON_PVP_MAKE_ROOM_CLASS = "btn_p_ty1"
     NUMBER_REMAINED_PVP_CSS = ".pvp_schedule > em > strong"
 
+    # Item
+    CONDITION_AND_INJURY_PLAYER0_CSS = "#plr_num0 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER1_CSS = "#plr_num1 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER2_CSS = "#plr_num2 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER3_CSS = "#plr_num3 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER4_CSS = "#plr_num4 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER5_CSS = "#plr_num5 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER6_CSS = "#plr_num6 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER7_CSS = "#plr_num7 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER8_CSS = "#plr_num8 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER9_CSS = "#plr_num9 > div > .plr_cdt"
+    CONDITION_AND_INJURY_PLAYER10_CSS = "#plr_num10 > div > .plr_cdt"
+    STAMINA_PLAYER0_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER1_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER2_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER3_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER4_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER5_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER6_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER7_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER8_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER9_CSS = "#plr_num0 > div > .point > span"
+    STAMINA_PLAYER10_CSS = "#plr_num0 > div > .point > span"
+
     def __init__(self, id, pw, pvp):
         self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, EXPLICITLY_WAIT_SECONDS)
@@ -205,8 +229,13 @@ class SoccerNight(object):
                 return
 
     def go_item(self):
+        # FIXME: Go lineup before this. And remove below 4 lines.
+        if self.current_hour == time.localtime().tm_hour or time.localtime().tm_min < 20 or time.localtime().tm_min > 40:
+            return
+
+        self.current_hour = time.localtime().tm_hour
+
         self.driver.get("http://fd.naver.com/gmc/main#item")
-        #TODO: Use item properly.
 
     def go_gift(self):
         self.driver.get("http://fd.naver.com/gmc/main#gift")
